745d7c0fcc598c90985ffc49973f7871617ed562
Frangelicomk <mk3987@aol.com>
2022-08-20 11:55:48 +0000

    added env.py for variables, fixed issue with responsivness on all pages. more details on readme.md file

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 import os
+
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+# from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
@@ -16,7 +17,7 @@ UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static', 'assets')
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.secret_key = 'PUsG,-0id*1DKs(LXS}yp8PVTf`jzc'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 mongo = PyMongo(app)
 
